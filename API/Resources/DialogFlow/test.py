a922a15bd510cbfd974b5d8583de2db45e17ed81
Pablo Martin <pablo.martin.ext@bayer.com>
2018-04-03 16:09:54 +0200

    Testing flask-assistant

--- API/Resources/DialogFlow/test.py
+++ API/Resources/DialogFlow/test.py
@@ -1,14 +1,17 @@
 from pprint import pprint
+
 from flask import request
-from flask_restplus import Resource
 from flask_assistant import tell
+
 from API.flaskWrapper import FlaskWrapper
 
 
-class TestResource(Resource):
+class TestResource():
 
     @FlaskWrapper.Assistant.action('test')
     def test(self, testParam):
+
+        print('In test resource')
 
         if testParam == 'test':
             msg = 'Escribiste "test"'
